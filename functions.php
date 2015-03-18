a5553ee7748aa092f335ca6799676e8a1f28ed90
Galen Gidman <galen@galengidman.com>
2015-03-18 17:11:18 -0500

    add theme support for TBF widgets

--- functions.php
+++ functions.php
@@ -33,5 +33,22 @@ function tbft_theme_support() {
 
   add_theme_support( 'title-tag' );
 
+  add_theme_support( 'tbf', array(
+    'widgets' => array(
+      'events' => array(
+        'fields' => array( 'title', 'number', 'thumbnail', 'excerpt', 'date', 'time', 'venue', 'address', 'map' )
+      ),
+      'locations' => array(
+        'fields' => array( 'title', 'thumbnail', 'excerpt', 'address', 'phone', 'times', 'map' )
+      ),
+      'people' => array(
+        'fields' => array( 'title', 'group', 'number', 'thumbnail', 'excerpt', 'position', 'phone', 'email', 'urls' )
+      ),
+      'sermons' => array(
+        'fields' => array( 'title', 'number', 'thumbnail', 'excerpt', 'date', 'media' )
+      )
+    )
+  ) );
+
 }
 add_action( 'after_setup_theme', 'tbft_theme_support' );
