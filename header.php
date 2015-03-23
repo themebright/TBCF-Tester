b74d28655e03ae1e9e75a69ab64c3de60e1740f8
Galen Gidman <galen@galengidman.com>
2015-03-23 15:57:02 -0500

    added a couple of extra constants

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
 <body <?php body_class( 'container' ); ?>>
   <nav class="navbar navbar-default" style="margin-top: 20px;">
     <div class="navbar-header">
-      <a href="<?php echo home_url( '/' ); ?>" class="navbar-brand">TBF Tester</a>
+      <a href="<?php echo home_url( '/' ); ?>" class="navbar-brand"><?php echo TBF_THEME_NAME; ?></a>
     </div>
   </nav>
+
+  <pre><strong>Theme Name</strong>: <?php echo TBF_THEME_NAME; ?><br>
+<strong>Theme Author</strong>: <?php echo TBF_THEME_AUTHOR; ?><br>
+<strong>Theme Version</strong>: <?php echo TBF_THEME_VERSION; ?></pre>
